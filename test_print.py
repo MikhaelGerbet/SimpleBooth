8f768bbfcb209560340dcb750a702cc1cd550333
Mikhael GERBET <frollon.noir@gmail.com>
2025-12-08 23:29:00 +0100

    fix

--- test_print.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Script de test pour l'impression"""
-import requests
-import sys
-
-photo = sys.argv[1] if len(sys.argv) > 1 else "effect_20251202_225355.jpg"
-r = requests.post('http://localhost:5000/print_photo', json={'photo_path': photo})
-print(r.text)
